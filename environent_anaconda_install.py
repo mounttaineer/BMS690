0e2b0f2d937059250838c467a9a4c4e867ed849e
mounttaineer <74799568+mounttaineer@users.noreply.github.com>
2022-07-25 14:44:44 -0400

    Add files via upload

---
+++ environent_anaconda_install.py
@@ -0,0 +1,17 @@
+Cando env-JHH246-hT216
+
+Numpy: installed (conda)
+pandas: installed (conda)
+scanpy: installed (conda)
+harmonypy: installed (pip)
+leidenalg: installed, conda install -c conda-forge leidenalg
+umap-learn: installed, 
+MulticoreTSNE: installed, conda install -c conda-forge multicore-tsne
+scikit-image:installed, conda install -c conda-forge scikit-image
+scikit-learn:installed, conda install -c conda-forge scikit-learn
+scvelo:installed,conda install -c bioconda scvelo
+vel0cyto requirements: conda install numpy scipy cython numba matplotlib scikit-learn h5py click
+pysam:pip install pysam
+velocyto:installed,pip install velocyto
+jupyterlab: installed
+nb_conda_kernels:installed, conda install -c conda-forge nb_conda_kernels
